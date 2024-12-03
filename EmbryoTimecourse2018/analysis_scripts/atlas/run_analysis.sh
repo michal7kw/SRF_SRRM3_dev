0c3d5e79984e47150b93c406e81048665744731f
michal7kw <michal7kw@gmail.com>
2024-12-04 00:29:28 +0100

    update

--- EmbryoTimecourse2018/analysis_scripts/atlas/run_analysis.sh
+++ EmbryoTimecourse2018/analysis_scripts/atlas/run_analysis.sh
@@ -8,38 +8,30 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kubacki.michal@hsr.it
-#SBATCH --error="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_SRRM3_Timecourse/EmbryoTimecourse2018/analysis_scripts/atlas/logs/run_analysis_atlas.err"
-#SBATCH --output="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_SRRM3_Timecourse/EmbryoTimecourse2018/analysis_scripts/atlas/logs/run_analysis_atlas.out"
+#SBATCH --error="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_SRRM3_dev/EmbryoTimecourse2018/analysis_scripts/atlas/logs/run_analysis_atlas.err"
+#SBATCH --output="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_SRRM3_dev/EmbryoTimecourse2018/analysis_scripts/atlas/logs/run_analysis_atlas.out"
 
 source /opt/common/tools/ric.cosr/miniconda3/bin/activate /beegfs/scratch/ric.broccoli/kubacki.michal/conda_envs/jupyter_nb
 
 # Set working directory
-WORKDIR="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_SRRM3_Timecourse/EmbryoTimecourse2018"
-DATA_DIR="${WORKDIR}/download/DATA"
+WORKDIR="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_SRRM3_dev/EmbryoTimecourse2018"
 cd ${WORKDIR}/analysis_scripts/atlas
 
-# # Extract data files if not already extracted
-# if [ ! -d "${DATA_DIR}/atlas" ]; then
-#     cd ${DATA_DIR}
-#     tar -xzf atlas_data.tar.gz
-#     tar -xzf chimera_tal1_data.tar.gz
-#     tar -xzf chimera_wt_data.tar.gz
-# fi
+# Create output directories
+mkdir -p vis/ribbon/plots
+mkdir -p vis/density
+mkdir -p vis/umap
 
-# Since you already have:
-# - raw_counts.mtx
-# - barcodes.tsv
-# - genes.tsv
-# - meta.tab
-# - corrected_pcas.rds
-# - sizefactors.tab
-# Skip to visualization and final analysis steps
-
-# Execute visualization scripts
+# Execute visualization scripts in order
+# 1. Density analysis
 Rscript -e "rmarkdown::render('vis/density/density.Rmd')"
-Rscript -e "rmarkdown::render('vis/ribbon/ribbon.Rmd')"
+
+# 2. UMAP calculation and visualization 
 Rscript -e "rmarkdown::render('vis/umap/umap.Rmd')"
 python vis/umap/umap.py
+
+# 3. Ribbon plots (depends on UMAP)
+Rscript -e "rmarkdown::render('vis/ribbon/ribbon.Rmd')"
 
 # Generate Shiny app data
 Rscript shiny/make_data.R
--- EmbryoTimecourse2018/analysis_scripts/atlas/vis/umap/umap.py
+++ EmbryoTimecourse2018/analysis_scripts/atlas/vis/umap/umap.py
@@ -1,29 +1,13 @@
 import numpy as np
-import scanpy as sc
+import umap
 
-#All cells
-pca = np.loadtxt("/nfs/research1/marioni/jonny/embryos/scripts/vis/umap/scanpy_input.tab")
-obj = sc.AnnData(np.random.random((pca.shape[0], 100)))
-obj.obsm["X_pca"] = pca
+# Load data
+pca = np.loadtxt("scanpy_input.tab")
 
-sc.pp.neighbors(obj, n_neighbors = 20, use_rep = "X_pca", random_state = 42)
-sc.tl.umap(obj, min_dist = 0.7, random_state = 42)
-out = obj.obsm["X_umap"]
-np.savetxt("/nfs/research1/marioni/jonny/embryos/scripts/vis/umap/umap.tab", out, delimiter="\t")
+# Run UMAP
+reducer = umap.UMAP(random_state=42)
+embedding = reducer.fit_transform(pca)
 
-# Tests
-# for i in np.arange(0.1, 0.8, 0.1):
-# 	sc.tl.umap(obj, min_dist = i, random_state = 42)
-# 	out = obj.obsm["X_umap"]
-# 	np.savetxt("/nfs/research1/marioni/jonny/embryos/scripts/vis/umap/mindist_" + str(i) + ".tab", out, delimiter="\t")
-
-# Embryonic cells
-pca = np.loadtxt("/nfs/research1/marioni/jonny/embryos/scripts/vis/umap/scanpy_input_embryonic.tab")
-obj = sc.AnnData(np.random.random((pca.shape[0], 100)))
-obj.obsm["X_pca"] = pca
-
-sc.pp.neighbors(obj, n_neighbors = 20, use_rep = "X_pca", random_state = 42)
-sc.tl.umap(obj, min_dist = 0.7, random_state = 42)
-out = obj.obsm["X_umap"]
-np.savetxt("/nfs/research1/marioni/jonny/embryos/scripts/vis/umap/umap_embryonic.tab", out, delimiter="\t")
+# Save results
+np.savetxt("umap.tab", embedding, delimiter="\t")
 
